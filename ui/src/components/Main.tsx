c0e126f6da416a32503f4ca138735fe3ea08698c
Marcin Gutka <gucios17@gmail.com>
2022-01-28 12:33:16 +0100

    Added pagination

--- ui/src/components/Main.tsx
+++ ui/src/components/Main.tsx
@@ -2,22 +2,71 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import axios from 'axios';
 
+import { Pagination } from './utils/Pagination';
+
 export const Main = () =>
 {
-  const [data, setData] = React.useState<IGetResult[]>();
+  const displayedItem: number = 10;
+  const [page, setPage] = React.useState(1);
+  const [pages, setPages] = React.useState(1);
+  const [data, setData] = React.useState<IGetResult[]>([]);
 
   React.useEffect(() => {
-    axios.get('https://localhost:7236/api/dataSet').then((response) => setData(response.data)).catch(() => console.log('failed'));
+    const fetchData = async () => {const res = await axios.get('https://localhost:7236/api/dataSet');setData(res.data)}; fetchData();
   }, []);
 
-    return (
+  React.useEffect(() => {
+    setPages(Math.ceil(data.length/displayedItem));
+  }, [data, displayedItem])
+
+  const isPaginated: boolean = data.length > displayedItem;
+  
+  var paginatedData: IGetResult[];
+
+  paginatedData = isPaginated? Paginate(data, page, displayedItem) : data;
+
+
+   return (
+    <React.Fragment>
       <Container>
         <Row>
-          {data?.map( x=> <Col>{x.name}</Col>)}
-          <Col>MArincc</Col>
+          <Col>Upload File</Col>
+          <Col>
+            <Row className="center-row">
+              <Col className="d-flex justify-content-center">Name</Col>
+              <Col className="d-flex justify-content-center">Date</Col>
+              <Col className="d-flex justify-content-center"></Col>
+            </Row>
+            {paginatedData.map((x) => { return (
+            <Row className="m-2">
+              <Col className="d-flex justify-content-center">
+                <button type="button" className="btn btn-link">{x.name}</button>
+              </Col>
+              <Col className="d-flex justify-content-center">
+                {x.generationDate}
+              </Col>
+              <Col className="d-flex justify-content-center">
+                <button type="button" className="btn btn-warning">DELETE</button>
+              </Col>
+            </Row>
+            )})}
+            {<Row>
+              <Col>
+                {isPaginated && <Pagination page={page} pages={pages} onClick={setPage} />}
+              </Col>
+            </Row>}
+          </Col>
         </Row>
+        Pages: {pages}
       </Container>
-    )
+    </React.Fragment>
+  )
+}
+
+function Paginate(data: IGetResult[], page: number, elementNumber: number): IGetResult[] {
+  const startIndex = (page - 1) * elementNumber;
+  const endIndex = data.length > startIndex + elementNumber ? startIndex + elementNumber : data.length;
+  return data.slice(startIndex, endIndex);
 }
 
 export interface IGetResult {
---
+++ ui/src/components/utils/Pagination.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+export const Pagination = (props: IPaginationProps) =>
+{
+  const {page, pages, onClick} = props;
+
+  var displayedPages: number[];
+
+  var buttons:JSX.Element;
+  
+  if (pages === 1) return null;
+  else if (pages <= 3 || page === 1)
+  {
+    buttons = 
+    <React.Fragment>
+      <li className="page-item ">
+        <button className="page-link" onClick={() => onClick(1)}>1</button>
+      </li>
+      <li className="page-item">
+        <button className="page-link" onClick={() => onClick(2)}>2</button>
+      </li>
+      {pages >= 3 && <li className="page-item">
+        <button className="page-link" onClick={() => onClick(3)}>3</button>
+      </li>}
+    </React.Fragment>
+  }
+  else if (page === pages)
+  {
+    buttons = 
+    <React.Fragment>
+      <li className="page-item ">
+        <button className="page-link" onClick={() => onClick(page - 2)}>{ page - 2 }</button>
+      </li>
+      <li className="page-item">
+        <button className="page-link" onClick={() => onClick(page - 1)}>{ page - 1 }</button>
+      </li>
+      {<li className="page-item">
+        <button className="page-link" onClick={() => onClick(page)}>{ page }</button>
+      </li>}
+    </React.Fragment>
+  }
+  else
+  {
+    buttons = 
+    <React.Fragment>
+      <li className="page-item ">
+        <button className="page-link" onClick={() => onClick(page - 1)}>{ page - 1 }</button>
+      </li>
+      <li className="page-item">
+        <button className="page-link" onClick={() => onClick(page)}>{ page }</button>
+      </li>
+      {<li className="page-item">
+        <button className="page-link" onClick={() => onClick(page + 1)}>{ page + 1 }</button>
+      </li>}
+    </React.Fragment>
+  }
+
+  return (
+    <React.Fragment>
+      <nav aria-label="...">
+        <ul className="pagination justify-content-center">
+          <li className="page-item">
+            <button className="page-link" onClick={() => onClick(1)}>First</button> 
+          </li>
+          {buttons}
+          <li className="page-item">
+            <button className="page-link" onClick={() => onClick(pages)}>Last</button>  
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  )
+}
+
+export interface IPaginationProps {
+  pages: number;
+  page: number;
+  onClick(page: number): any; 
+}

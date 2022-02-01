925eb168793755f6d349a2ca45da7daf5f0fd705
Marcin Gutka <gucios17@gmail.com>
2022-02-01 21:21:10 +0100

    Fix naming files

--- ui/src/common/downloadHandler.ts
+++ ui/src/common/downloadHandler.ts
@@ -3,12 +3,14 @@ import { GetRunResults, GetRunInput } from "../services/DataSetApiRequest";
 
 export const resultsDownloadHandler = async (runId: string) => {
     const response = await GetRunResults(runId);
+    const fileNameAnnex = " - Result.txt"
     const fileName = response.headers['content-disposition'].split('filename=')[1].split(';')[0];
-    FileDownload(response.data, fileName? fileName : {runId} + "- Result.txt");
+    FileDownload(response.data, fileName? fileName.replace(".txt","") + fileNameAnnex : {runId} + fileNameAnnex);
 }
 
 export const inputDownloadHandler = async (runId: string) => {
     const response = await GetRunInput(runId);
+    const fileNameAnnex = " - Input.txt"
     const fileName = response.headers['content-disposition'].split('filename=')[1].split(';')[0];
-    FileDownload(response.data, fileName? fileName : {runId} + " - Input.txt");
+    FileDownload(response.data, fileName? fileName.replace(".txt","") + fileNameAnnex : {runId} + fileNameAnnex);
 }

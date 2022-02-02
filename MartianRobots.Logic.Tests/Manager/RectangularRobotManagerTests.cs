177c1503603ac0e6bfc4edc304d978db3c792c69
Marcin Gutka <gucios17@gmail.com>
2022-02-02 16:32:35 +0100

    Added unit test fro manager

--- MartianRobots.Logic.Tests/Manager/RectangularRobotManagerTests.cs
+++ MartianRobots.Logic.Tests/Manager/RectangularRobotManagerTests.cs
@@ -30,7 +30,7 @@ namespace MartianRobots.Logic.Tests
         }
 
         [Test]
-        public async Task Check_If_ExecuteTasksAsync_Assign_New_Position_When_Robot_Has_Not_Been_Lost()
+        public async Task ExecuteTasksAsync_Assign_New_Position_When_Robot_Has_Not_Been_OffGrid()
         {
             //Arrange
             var robotId = 1;
@@ -83,7 +83,61 @@ namespace MartianRobots.Logic.Tests
         }
 
         [Test]
-        public async Task Check_If_ExecuteTasksAsync_Do_Not_Assign_New_Position_When_Robot_Has_Been_Lost()
+        public async Task ExecuteTasksAsync_Do_Not_Assign_New_Position_When_Robot_Has_Been_OffGrid()
+        {
+            //Arrange
+            var robotId = 1;
+            var runName = "TestRun";
+            var grid = new Grid(6, 6);
+            var initialRobotPosition = new GridPosition()
+            {
+                X = 6,
+                Y = 6,
+                Orientation = OrientationState.North
+            };
+
+            var robots = new List<Robot>
+            {
+                new Robot
+                {
+                    Id = robotId,
+                    Position = initialRobotPosition
+                }
+            };
+
+            var commands = new List<RectangularMoveCommand>
+            {
+                RectangularMoveCommand.Forward
+            };
+
+            var robotCommands = new List<RobotCommands>
+            {
+                new RobotCommands
+                {
+                    Id = robotId,
+                    Commands = commands
+                }
+            };
+
+            cmdExecuter.Execute(default, default).ReturnsForAnyArgs(new GridPosition { X = 6, Y = 7, Orientation = OrientationState.North });
+            positionValidator.IsRobotOffGrid(default, default).ReturnsForAnyArgs(true);
+            positionValidator.IsRobotLost(default, default).ReturnsForAnyArgs(false);
+
+            manager.AssignGridAndRobots(grid, robots, robotCommands, runName);
+
+            //Act
+            await manager.ExecuteTasksAsync();
+
+            //Assert
+            var currentPosition = robots.First(r => r.Id == robotId).Position;
+
+            Assert.AreEqual(initialRobotPosition.X, currentPosition.X);
+            Assert.AreEqual(initialRobotPosition.Y, currentPosition.Y);
+            Assert.AreEqual(initialRobotPosition.Orientation, currentPosition.Orientation);
+        }
+
+        [Test]
+        public async Task ExecuteTasksAsync_Do_Not_Assign_New_Position_When_Robot_Has_Been_Lost()
         {
             //Arrange
             var robotId = 1;
@@ -137,7 +191,7 @@ namespace MartianRobots.Logic.Tests
         }
 
         [Test]
-        public async Task Check_If_ExecuteTasksAsync_Set_IsLost_To_True_When_Robot_Has_Been_Lost()
+        public async Task ExecuteTasksAsync_Set_IsLost_To_True_When_Robot_Has_Been_Lost()
         {
             //Arrange
             var robotId = 1;
@@ -184,6 +238,136 @@ namespace MartianRobots.Logic.Tests
 
             //Assert
             Assert.IsTrue(robots.First(r => r.Id == robotId).IsLost);
+        }
+
+        [Test]
+        public async Task ExecuteTasksAsync_Execute_Next_Command_When_Robot_Was_About_To_Be_OffGrid_But_Position_Was_Already_Marked()
+        {
+            //Arrange
+            var robotId = 1;
+            var secondRobotId = 2;
+            var runName = "TestRun";
+            var grid = new Grid(6, 6);
+            var initialRobotPosition = new GridPosition()
+            {
+                X = 6,
+                Y = 6,
+                Orientation = OrientationState.North
+            };
+
+            var robots = new List<Robot>
+            {
+                new Robot
+                {
+                    Id = robotId,
+                    Position = initialRobotPosition
+                },
+                new Robot
+                {
+                    Id = secondRobotId,
+                    Position = initialRobotPosition
+                }
+            };
+
+            var commands = new List<RectangularMoveCommand>
+            {
+                RectangularMoveCommand.Forward,
+                RectangularMoveCommand.Left
+            };
+
+            var robotCommands = new List<RobotCommands>
+            {
+                new RobotCommands
+                {
+                    Id = robotId,
+                    Commands = commands
+                },
+                new RobotCommands
+                {
+                    Id = secondRobotId,
+                    Commands = commands
+                }
+            };
+
+            cmdExecuter.Execute(Arg.Any<GridPosition>(), Arg.Is<RectangularMoveCommand>(x => x == RectangularMoveCommand.Forward)).Returns(new GridPosition { X = 6, Y = 7, Orientation = OrientationState.North });
+            cmdExecuter.Execute(Arg.Any<GridPosition>(), Arg.Is<RectangularMoveCommand>(x => x == RectangularMoveCommand.Left)).Returns(new GridPosition { X = 6, Y = 6, Orientation = OrientationState.West });
+            positionValidator.IsRobotOffGrid(Arg.Is<GridPosition>(x => x.Orientation == OrientationState.North), Arg.Any<Grid>()).Returns(true);
+            positionValidator.IsRobotOffGrid(Arg.Is<GridPosition>(x => x.Orientation == OrientationState.West), Arg.Any<Grid>()).Returns(false);
+            positionValidator.IsRobotLost(Arg.Any<GridPosition>(), Arg.Is<List<Position>>(x => x.Count == 0)).Returns(true);
+            positionValidator.IsRobotLost(Arg.Any<GridPosition>(), Arg.Is<List<Position>>(x => x.Count > 0)).Returns(false);
+
+            manager.AssignGridAndRobots(grid, robots, robotCommands, runName);
+
+            //Act
+            await manager.ExecuteTasksAsync();
+
+            //Assert
+            Assert.IsTrue(robots.First(r => r.Id == secondRobotId).Position.Orientation == OrientationState.West);
+        }
+
+        [Test]
+        public async Task ExecuteTasksAsync_Do_Not_Assign_IsLost_When_Robot_Was_About_To_Be_OffGrid_But_Position_Was_Already_Marked()
+        {
+            //Arrange
+            var robotId = 1;
+            var secondRobotId = 2;
+            var runName = "TestRun";
+            var grid = new Grid(6, 6);
+            var initialRobotPosition = new GridPosition()
+            {
+                X = 6,
+                Y = 6,
+                Orientation = OrientationState.North
+            };
+
+            var robots = new List<Robot>
+            {
+                new Robot
+                {
+                    Id = robotId,
+                    Position = initialRobotPosition
+                },
+                new Robot
+                {
+                    Id = secondRobotId,
+                    Position = initialRobotPosition
+                }
+            };
+
+            var commands = new List<RectangularMoveCommand>
+            {
+                RectangularMoveCommand.Forward,
+                RectangularMoveCommand.Left
+            };
+
+            var robotCommands = new List<RobotCommands>
+            {
+                new RobotCommands
+                {
+                    Id = robotId,
+                    Commands = commands
+                },
+                new RobotCommands
+                {
+                    Id = secondRobotId,
+                    Commands = commands
+                }
+            };
+
+            cmdExecuter.Execute(Arg.Any<GridPosition>(), Arg.Is<RectangularMoveCommand>(x => x == RectangularMoveCommand.Forward)).Returns(new GridPosition { X = 6, Y = 7, Orientation = OrientationState.North });
+            cmdExecuter.Execute(Arg.Any<GridPosition>(), Arg.Is<RectangularMoveCommand>(x => x == RectangularMoveCommand.Left)).Returns(new GridPosition { X = 6, Y = 6, Orientation = OrientationState.West });
+            positionValidator.IsRobotOffGrid(Arg.Is<GridPosition>(x => x.Orientation == OrientationState.North), Arg.Any<Grid>()).Returns(true);
+            positionValidator.IsRobotOffGrid(Arg.Is<GridPosition>(x => x.Orientation == OrientationState.West), Arg.Any<Grid>()).Returns(false);
+            positionValidator.IsRobotLost(Arg.Any<GridPosition>(), Arg.Is<List<Position>>(x => x.Count == 0)).Returns(true);
+            positionValidator.IsRobotLost(Arg.Any<GridPosition>(), Arg.Is<List<Position>>(x => x.Count > 0)).Returns(false);
+
+            manager.AssignGridAndRobots(grid, robots, robotCommands, runName);
+
+            //Act
+            await manager.ExecuteTasksAsync();
+
+            //Assert
+            Assert.IsFalse(robots.First(r => r.Id == secondRobotId).IsLost);
         }
     }
 }

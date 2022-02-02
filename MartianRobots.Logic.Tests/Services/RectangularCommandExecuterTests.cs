6daba4cb1d85a3daed2905eb75e0412b7d0bd0e1
Marcin Gutka <gucios17@gmail.com>
2022-02-02 17:20:15 +0100

    Added test for right command

--- MartianRobots.Logic.Tests/Services/RectangularCommandExecuterTests.cs
+++ MartianRobots.Logic.Tests/Services/RectangularCommandExecuterTests.cs
@@ -190,5 +190,93 @@ namespace MartianRobots.Logic.Tests.Services
             Assert.AreEqual(3, result.Y);
             Assert.AreEqual(OrientationState.North, result.Orientation);
         }
+
+        [Test]
+        public void Execute_When_Orientation_Is_Equal_North_And_Command_Is_Equal_Right_Returns_Orientation_Equals_East()
+        {
+            //Arrange
+            var initialPosition = new GridPosition()
+            {
+                X = 3,
+                Y = 3,
+                Orientation = OrientationState.North
+            };
+
+            var command = RectangularMoveCommand.Right;
+
+            //Act
+            var result = commandExecuter.Execute(initialPosition, command);
+
+            //Assert
+            Assert.AreEqual(3, result.X);
+            Assert.AreEqual(3, result.Y);
+            Assert.AreEqual(OrientationState.East, result.Orientation);
+        }
+
+        [Test]
+        public void Execute_When_Orientation_Is_Equal_East_And_Command_Is_Equal_Right_Returns_Orientation_Equals_South()
+        {
+            //Arrange
+            var initialPosition = new GridPosition()
+            {
+                X = 3,
+                Y = 3,
+                Orientation = OrientationState.East
+            };
+
+            var command = RectangularMoveCommand.Right;
+
+            //Act
+            var result = commandExecuter.Execute(initialPosition, command);
+
+            //Assert
+            Assert.AreEqual(3, result.X);
+            Assert.AreEqual(3, result.Y);
+            Assert.AreEqual(OrientationState.South, result.Orientation);
+        }
+
+        [Test]
+        public void Execute_When_Orientation_Is_Equal_South_And_Command_Is_Equal_Right_Returns_Orientation_Equals_West()
+        {
+            //Arrange
+            var initialPosition = new GridPosition()
+            {
+                X = 3,
+                Y = 3,
+                Orientation = OrientationState.South
+            };
+
+            var command = RectangularMoveCommand.Right;
+
+            //Act
+            var result = commandExecuter.Execute(initialPosition, command);
+
+            //Assert
+            Assert.AreEqual(3, result.X);
+            Assert.AreEqual(3, result.Y);
+            Assert.AreEqual(OrientationState.West, result.Orientation);
+        }
+
+        [Test]
+        public void Execute_When_Orientation_Is_Equal_West_And_Command_Is_Equal_Right_Returns_Orientation_Equals_North()
+        {
+            //Arrange
+            var initialPosition = new GridPosition()
+            {
+                X = 3,
+                Y = 3,
+                Orientation = OrientationState.West
+            };
+
+            var command = RectangularMoveCommand.Right;
+
+            //Act
+            var result = commandExecuter.Execute(initialPosition, command);
+
+            //Assert
+            Assert.AreEqual(3, result.X);
+            Assert.AreEqual(3, result.Y);
+            Assert.AreEqual(OrientationState.North, result.Orientation);
+        }
     }
 }
